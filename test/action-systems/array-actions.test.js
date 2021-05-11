0a56f08dfeaec8b4e5cbff68b4ee4c925abcfc67
caperaven <caperaven@gmail.com>
2021-05-11 17:31:00 +0200

    initial

--- test/action-systems/array-actions.test.js
+++ test/action-systems/array-actions.test.js
@@ -1,10 +1,17 @@
 import "./../../src/index.js";
+import {getValueOnPath} from "./../mockups/binding-mocks.js";
 
 let log = null;
 
 beforeAll(() => {
     globalThis.console = {
         error: (msg) => log = msg
+    }
+
+    globalThis.crsbinding = {
+        utils: {
+            getValueOnPath: getValueOnPath
+        }
     }
 })
 
@@ -20,4 +27,14 @@ test("ArrayActions - add - no array - show error", async () => {
     const values = null;
     await globalThis.crs.intent.array.perform({action: "add", args: {target: values, value: "Hello World"}});
     expect(log).not.toBeNull();
+})
+
+test("ArrayActions - add - on context path", async () => {
+    const context = {
+        values: []
+    }
+
+    await globalThis.crs.intent.array.perform({action: "add", args: {target: "@context.values", value: "Hello World"}}, context);
+    expect(context.values.length).toEqual(1);
+    expect(context.values[0]).toEqual("Hello World");
 })
