4ac3c6be2339707117f686cbc6ee428164aef218
caperaven <caperaven@gmail.com>
2021-11-29 18:34:50 +0200

    added equal and not equal expressions

--- wasm/data/src/evaluators/not_equal.rs
+++ wasm/data/src/evaluators/not_equal.rs
@@ -6,7 +6,7 @@ pub struct NotEqual {}
 
 impl Eval for NotEqual {
     fn evaluate(obj1: Value, obj2: Value) -> bool {
-        eval!(obj1, ==, obj2)
+        eval!(obj1, !=, obj2)
     }
 }
 
@@ -18,19 +18,19 @@ mod test {
 
     #[test]
     fn test_positive() {
-        assert_eq!(NotEqual::evaluate(Value::from(20), Value::from(20)), true);
-        assert_eq!(NotEqual::evaluate(Value::from(20.0), Value::from(20.0)), true);
-        assert_eq!(NotEqual::evaluate(Value::from(true), Value::from(true)), true);
-        assert_eq!(NotEqual::evaluate(Value::from("B"), Value::from("B")), true);
-        assert_eq!(NotEqual::evaluate(Value::from("100"), Value::from("100")), true);
+        assert_eq!(NotEqual::evaluate(Value::from(20), Value::from(20)), false);
+        assert_eq!(NotEqual::evaluate(Value::from(20.0), Value::from(20.0)), false);
+        assert_eq!(NotEqual::evaluate(Value::from(true), Value::from(true)), false);
+        assert_eq!(NotEqual::evaluate(Value::from("B"), Value::from("B")), false);
+        assert_eq!(NotEqual::evaluate(Value::from("100"), Value::from("100")), false);
     }
 
     #[test]
     fn test_negative() {
-        assert_eq!(NotEqual::evaluate(Value::from(10), Value::from(20)), false);
-        assert_eq!(NotEqual::evaluate(Value::from(10.0), Value::from(20.0)), false);
-        assert_eq!(NotEqual::evaluate(Value::from(false), Value::from(true)), false);
-        assert_eq!(NotEqual::evaluate(Value::from("A"), Value::from("B"), ), false);
-        assert_eq!(NotEqual::evaluate(Value::from("001"), Value::from("100")), false);
+        assert_eq!(NotEqual::evaluate(Value::from(10), Value::from(20)), true);
+        assert_eq!(NotEqual::evaluate(Value::from(10.0), Value::from(20.0)), true);
+        assert_eq!(NotEqual::evaluate(Value::from(false), Value::from(true)), true);
+        assert_eq!(NotEqual::evaluate(Value::from("A"), Value::from("B"), ), true);
+        assert_eq!(NotEqual::evaluate(Value::from("001"), Value::from("100")), true);
     }
 }
