ea26633e3278b169c600f7a86a7620e893d52406
caperaven <caperaven@gmail.com>
2021-11-29 18:33:52 +0200

    added equal and not equal expressions

--- wasm/data/src/evaluators.rs
+++ wasm/data/src/evaluators.rs
@@ -2,8 +2,12 @@ mod greater_than;
 mod less_than;
 mod greater_or_equal;
 mod less_or_equal;
+mod equal;
+mod not_equal;
 
 pub use greater_than::GreaterThan;
 pub use less_than::LessThan;
 pub use greater_or_equal::GreaterOrEqual;
 pub use less_or_equal::LessOrEqual;
+pub use equal::Equal;
+pub use not_equal::NotEqual;
