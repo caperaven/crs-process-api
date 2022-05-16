0a411fc9cfab4bfa8077655eb8ea182fad0273f8
caperaven <caperaven@gmail.com>
2022-05-16 11:35:20 +0200

    update dom to better handle get element

---
+++ app/media/media.js
@@ -0,0 +1,5 @@
+export default class Media extends crsbinding.classes.ViewBase {
+    async connectedCallback() {
+        await super.connectedCallback();
+    }
+}
--- src/action-systems/media-actions.js
+++ src/action-systems/media-actions.js
@@ -5,6 +5,15 @@ export class MediaActions {
 
     static async render_camera(step, context, process, item) {
         const canvas = await crs.get_element(step, context, process, item);
+        let constraints = await crs.process.getValue(step.args.constraints, context, process, item);
 
+        if (constraints == null) {
+            constraints = {
+                video: true
+            }
+        }
+
+        const stream = await navigator.mediaDevices.getUserMedia(constraints);
+        canvas.srcObject = stream;
     }
 }
