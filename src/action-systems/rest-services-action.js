c9a0f502c8d303f0850ac881675e9854503127f8
caperaven <caperaven@gmail.com>
2022-06-01 17:27:07 +0200

    add rest api feature

--- src/action-systems/rest-services-action.js
+++ src/action-systems/rest-services-action.js
@@ -1,56 +1,88 @@
+const BASE_REQUEST = {
+    cache: "default",
+    mode: "cors",
+    credentials: 'same-origin',
+    referrerPolicy: 'no-referrer',
+    redirect: 'follow',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+}
+
 export class RestServicesAction {
     static async perform(step, context, process, item) {
         await this[step.action]?.(step, context, process, item);
     }
 
     static async get(step, context, process, item) {
-        let request = step.args.request || {
-            cache: "default",
-            method: "GET",
-            mode: "cors",
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }
+        let request = await crs.process.getValue(step.args.request, context, process, item);
+        const url = await crs.process.getValue(step.args.url, context, process, item);
 
-        let result = await fetch(step.args.url, request).then(result => result.json());
+        request = request || Object.assign({
+            method: "GET"
+        }, BASE_REQUEST)
 
-        if (step.args.target != null) {
-            await crs.process.setValue(step.args.target, result, context, process, item);
-        }
-
-        return result;
+        const result = await fetch(url, request).then(result => result.json());
+        return setTarget(step, result, context, process, item);
     }
 
     static async post(step, context, process, item) {
+        const url = await crs.process.getValue(step.args.url, context, process, item);
+        const body = await crs.process.getValue(step.args.body);
+        let request = await crs.process.getValue(step.args.request, context, process, item);
 
+        request = request || Object.assign({
+            body: body,
+            method: "POST"
+        }, BASE_REQUEST);
+
+        const result = await fetch(url, request).then(result => result.json())
+        return setTarget(step, result, context, process, item);
     }
 
     static async put(step, context, process, item) {
+        const url = await crs.process.getValue(step.args.url, context, process, item);
+        const body = await crs.process.getValue(step.args.body);
+        let request = await crs.process.getValue(step.args.request, context, process, item);
 
+        request = request || Object.assign({
+            body: body,
+            method: "PUT"
+        }, BASE_REQUEST);
+
+        const result = await fetch(url, request).then(result => result.json())
+        return setTarget(step, result, context, process, item);
     }
 
     static async patch(step, context, process, item) {
-        let request = step.args.request || {
-            body: step.args.body,
-            cache: "default",
-            method: "PATCH",
-            mode: "cors",
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }
+        const body = await crs.process.getValue(step.args.body);
+        let request = await crs.process.getValue(step.args.request);
 
-        let result = await fetch(step.args.url, request).then(result => result.json());
+        request = request || Object.assign({
+            body: body,
+            method: "PATCH"
+        }, BASE_REQUEST);
 
-        if (step.args.target != null) {
-            await crs.process.setValue(step.args.target, result, context, process, item);
-        }
-
-        return result;
+        const result = await fetch(step.args.url, request).then(result => result.json());
+        return setTarget(step, result, context, process, item);
     }
 
     static async delete(step, context, process, item) {
+        let request = await crs.process.getValue(step.args.request);
 
+        request = request || Object.assign({
+            method: "DELETE"
+        }, BASE_REQUEST);
+
+        const result = await fetch(step.args.url, request).then(result => result.json());
+        return setTarget(step, result, context, process, item);
     }
+}
+
+async function setTarget(step, result, context, process, item) {
+    if (step.args.target != null) {
+        await crs.process.setValue(step.args.target, result, context, process, item);
+    }
+
+    return result;
 }
