68b9fb46ec6044597bb0c5e733c970386df0510d
caperaven <caperaven@gmail.com>
2022-09-19 20:55:48 +0200

    add normalize to math

--- src/action-systems/colors-actions.js
+++ src/action-systems/colors-actions.js
@@ -6,8 +6,12 @@ export class ColorsActions {
     }
 
     static async hex_to_rgb(step, context, process, item) {
+        const normalized = (await crs.process.getValue(step.args.normalized, context, process, item) || false);
         const hex = await crs.process.getValue(step.args.hex, context, process, item);
         const result = hexToRgb(hex);
+
+        if (normalized == true) {
+        }
 
         if (step.args.target != null) {
             await crs.process.setValue(step.args.value, result, context, process, item);
@@ -20,6 +24,8 @@ export class ColorsActions {
         const r = (await crs.process.getValue(step.args.r, context, process, item)) || 0;
         const g = (await crs.process.getValue(step.args.g, context, process, item)) || 0;
         const b = (await crs.process.getValue(step.args.b, context, process, item)) || 0;
+        const normalized = (await crs.process.getValue(step.args.normalized, context, process, item) || false);
+
         const result = `#${decimalToHex(r)}${decimalToHex(g)}${decimalToHex(b)}`;
 
         if (step.args.target != null) {
@@ -35,7 +41,7 @@ function decimalToHex(c) {
     return hex.length == 1 ? "0" + hex : hex;
 }
 
-function hexToRgb(hex) {
+function hexToRgb(hex, factor) {
     const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
     return result ? {
         r: parseInt(result[1], 16),
