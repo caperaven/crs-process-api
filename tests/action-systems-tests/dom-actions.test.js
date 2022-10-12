e01e84f326ae5446272a491e37bd933d66acfd22
caperaven <caperaven@gmail.com>
2022-10-12 18:11:50 +0200

    add dom tests

--- tests/action-systems-tests/dom-actions.test.js
+++ tests/action-systems-tests/dom-actions.test.js
@@ -1,6 +1,6 @@
 import { beforeAll} from "https://deno.land/std@0.157.0/testing/bdd.ts";
 import {ElementMock} from "../mockups/element-mock.js";
-import { assertEquals, assertNotEquals } from "https://deno.land/std@0.147.0/testing/asserts.ts";
+import { assertEquals, assertNotEquals, assert } from "https://deno.land/std@0.147.0/testing/asserts.ts";
 import {init} from "./../mockups/init.js";
 
 await init();
@@ -56,4 +56,88 @@ Deno.test("remove_class", async () => {
     })
 
     assertEquals(element.classList.contains("blue"), false);
+})
+
+Deno.test("set_style", async () => {
+    const element = new ElementMock("div");
+
+    await crs.call("dom", "set_style", {
+        element: element,
+        style: "background",
+        value: "blue"
+    })
+
+    assertEquals(element.style.background, "blue");
+})
+
+Deno.test("set_styles", async () => {
+    const element = new ElementMock("div");
+
+    await crs.call("dom", "set_styles", {
+        element: element,
+        styles: {
+            background: "blue",
+            color: "white"
+        }
+    })
+
+    assertEquals(element.style.background, "blue");
+    assertEquals(element.style.color, "white");
+})
+
+Deno.test("set_text", async () => {
+    const element = new ElementMock("div");
+
+    await crs.call("dom", "set_text", {
+        element: element,
+        value: "Hello World"
+    })
+
+    assertEquals(element.textContent, "Hello World");
+})
+
+Deno.test("get_text", async () => {
+    const element = new ElementMock("div");
+    element.textContent = "Hello World";
+
+    const text = await crs.call("dom", "get_text", {
+        element: element
+    })
+
+    assertEquals(text, "Hello World");
+})
+
+Deno.test("create_element", async () => {
+    const parent = new ElementMock("div");
+    const element = await crs.call("dom", "create_element", {
+        parent: parent,
+        tag_name: "label",
+        attributes: {
+            for: "edtFirstName"
+        },
+        styles: {
+            background: "blue"
+        },
+        classes: ["italic"],
+        dataset: {
+            id: "lblFirstName"
+        },
+        text_content: "First Name",
+        children: [
+            {
+                tag_name: "input"
+            }
+        ]
+    });
+
+    assert(element != null);
+    assertEquals(parent.children.length, 1);
+    assertEquals(element.nodeName, "LABEL");
+    assertEquals(element.getAttribute("for"), "edtFirstName");
+    assertEquals(element.style.background, "blue");
+    assertEquals(element.classList.contains("italic"), true);
+    assertEquals(element.dataset.id, "lblFirstName");
+    assertEquals(element.textContent, "First Name");
+    assertEquals(element.children.length, 1);
+    assertEquals(element.children[0].nodeName, "INPUT");
 })
