8f3236e15dc13521d6d35aa4e4b75a7bc3a785fb
caperaven <caperaven@gmail.com>
2022-10-20 11:34:16 +0200

    compile if_value

---
+++ src/action-systems/compile-actions.js
@@ -0,0 +1,39 @@
+export class CompileActions {
+    static async perform(step, context, process, item) {
+        await this[step.action]?.(step, context, process, item);
+    }
+
+    static async if_value(step, context, process, item) {
+        // "model.value == 10 ? true : false"
+        const code = [];
+        const exp = await crs.process.getValue(step.args.exp, context, process, item);
+        const parts = exp.split("?")
+        const left = parts[0].trim();
+        const right = parts[1].trim();
+        const rightParts = right.split(":");
+
+        code.push(`if (${left}) {`);
+        code.push(`    return ${rightParts[0].trim()};`);
+        code.push('}');
+
+        if (rightParts.length > 1) {
+            code.push("else {");
+            code.push(`    return ${rightParts[1].trim()};`);
+            code.push("}");
+        }
+
+        const fn = new Function("model", code.join("\n"));
+
+        if (step.args.target != null) {
+            await crs.process.setValue(step.args.target, fn, context, process, item);
+        }
+
+        return fn;
+    }
+
+    static async case_value(step, context, process, item) {
+
+    }
+}
+
+crs.intent.compile = CompileActions;
---
+++ tests/action-systems-tests/compile-actions.test.js
@@ -0,0 +1,14 @@
+import { assertEquals } from "https://deno.land/std@0.147.0/testing/asserts.ts";
+import {ElementMock} from "../mockups/element-mock.js";
+import {init} from "./../mockups/init.js";
+
+await init();
+
+Deno.test("compile actions - if", async () => {
+    const result = await crs.call("compile", "if_value", {exp: "model.value == 10 ? true : false"});
+    const trueValue = result({value: 10});
+    const falseValue = result({value: 20});
+
+    assertEquals(trueValue, true);
+    assertEquals(falseValue, false);
+})
