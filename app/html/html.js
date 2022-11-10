004acdc3567802bbc06036056bee1a290d84980d
Ashton Angel <ashton.angel@pragmaworld.net>
2022-11-10 15:20:36 +0200

    Update html.js

--- app/html/html.js
+++ app/html/html.js
@@ -5,13 +5,15 @@ export default class Html extends crsbinding.classes.ViewBase {
 
     async create() {
         const number = this.amount.value ?? 1;
+        const fragment = document.createDocumentFragment();
         for (let l=0; l< number;l++) {
             const content = await crs.call("html", "create",
                 {
                     html: "<div><span>${test}</span>&nbsp;<span>${test2}</span></div>",
                     ctx: {test: "Hello", test2: "World!"}
                 });
-            this.element.appendChild(content);
+            fragment.appendChild(content);
         }
+        this.element.appendChild(fragment);
     }
 }
