ed938c6726567c0e1ee4db93c07c1d9fb156b36e
Ashton Angel <ashton.angel@pragmaworld.net>
2022-11-10 16:24:38 +0200

    better test page

--- app/html/html.js
+++ app/html/html.js
@@ -1,19 +1,24 @@
 export default class Html extends crsbinding.classes.ViewBase {
     async connectedCallback() {
         await super.connectedCallback();
+        this.html.value = "<div><span>${test}</span>&nbsp;<span>${test2}</span></div>";
+        this.context.value = `{"test": "Hello", "test2": "World!"}`;
     }
 
     async create() {
+
+        const contextObj = JSON.parse(this.context.value);
+
         const number = this.amount.value ?? 1;
         const fragment = document.createDocumentFragment();
         for (let l=0; l< number;l++) {
             const content = await crs.call("html", "create",
                 {
-                    html: "<div><span>${test}</span>&nbsp;<span>${test2}</span></div>",
-                    ctx: {test: "Hello", test2: "World!"}
+                    html: this.html.value,
+                    ctx: contextObj
                 });
             fragment.appendChild(content);
         }
-        this.element.appendChild(fragment);
+        this.container.appendChild(fragment);
     }
 }
