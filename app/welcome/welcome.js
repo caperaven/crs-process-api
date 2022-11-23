399a087f37e7982c14467a580a7bcb860859f326
caperaven <caperaven@gmail.com>
2022-11-23 13:04:38 +0200

    add search for the process api main page

--- app/welcome/welcome.js
+++ app/welcome/welcome.js
@@ -1,6 +1,48 @@
 export default class Welcome extends crsbinding.classes.ViewBase {
     async connectedCallback() {
         await super.connectedCallback();
-        await crs.call("compile", "if-value", {exp: "value == 10 ? true : false"})
+        await this.buildItems();
+    }
+
+    async buildItems() {
+        for (const module of Object.keys(crs.modules.registry)) {
+            await crs.modules.get(module);
+        }
+
+        const fragment = document.createDocumentFragment();
+
+        for (const system of Object.keys(crs.intent)) {
+            const cls = crs.intent[system];
+            for (const action of Object.getOwnPropertyNames(cls)) {
+                if (typeof cls[action] == "function") {
+                    const li = await crs.call("dom", "create_element", {
+                        parent: fragment,
+                        tag_name: "li",
+                        dataset: {
+                            tags: `${system} ${action}`
+                        },
+                        children: [
+                            {
+                                tag_name: "div",
+                                text_content: system
+                            },
+                            {
+                                tag_name: "div",
+                                text_content: action
+                            }
+                        ]
+                    })
+                }
+            }
+        }
+
+        this._element.querySelector("ul").appendChild(fragment);
+    }
+
+    async filter(event) {
+        await crs.call("dom_collection", "filter_children", {
+            element: this.collection,
+            filter: event.target.value
+        })
     }
 }
