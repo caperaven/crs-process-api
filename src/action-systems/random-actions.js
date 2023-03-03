215f62c9322337e41c6ef70fdc410653e92d8b0a
caperaven <caperaven@gmail.com>
2023-03-03 10:40:10 +0200

    add stubs random data

--- src/action-systems/random-actions.js
+++ src/action-systems/random-actions.js
@@ -12,6 +12,60 @@ export class RandomActions {
         return await this[step.action](step, context, process, item);
     }
 
+
+    /**
+     * @method generate_object - Generate an object with defined fields but using random values based on the field type
+     * @param step {object} - The step object that is being executed.
+     * @param context {object} - The context object that is passed to the process.
+     * @param process {object} - The process object
+     * @param item {object} - The item that is being processed.
+     *
+     * @param step.args.definition {object} - The definition of the object to generate.
+     * @returns {Promise<void>}
+     *
+     * @example <caption>javascript example</caption>
+     * const result = await crs.call("random", "generate_object", {
+     *     definition: {
+     *         id: "auto",
+     *         code: "string:auto",
+     *         description: "string:10",
+     *         price: "float:1:100",
+     *         quantity: "integer:1:100",
+     *         date: "date:2020-01-01:2020-12-31",
+     *         time: "time:0:24",
+     *         duration: "duration:1:10",
+     *         isValid: "boolean"
+     *     }
+     * })
+     */
+    static async generate_object(step, context, process, item) {
+        const definition = await crs.process.getValue(step.args.definition, context, process, item);
+    }
+
+    /**
+     * @method generate_collection - Generate a collection of objects with defined fields but using random values based on the field type
+     * @param step {object} - The step object that is being executed.
+     * @param context {object} - The context object that is passed to the process.
+     * @param process {object} - The process object
+     * @param item {object} - The item that is being processed.
+     *
+     * @param step.args.definition {object} - The definition of the object to generate.
+     * @param step.args.count {number} - The number of objects to generate.
+     * @returns {Promise<void>}
+     *
+     * @example <caption>javascript example</caption>
+     * const result = await crs.call("random", "generate_collection", {
+     *    definition: {
+     *      ... see generate_object
+     *    },
+     *    count: 100
+     * })
+     */
+    static async generate_collection(step, context, process, item) {
+        const definition = await crs.process.getValue(step.args.definition, context, process, item);
+        const count = await crs.process.getValue(step.args.count, context, process, item);
+    }
+
     /**
      * @method integer - Generate a random integer between the min and max values
      * @param step {object} - The step object that is being executed.
@@ -89,6 +143,18 @@ export class RandomActions {
             await crs.process.setValue(step.args.target, result, context, process, item);
         }
         return result;
+    }
+
+    static async string(step, context, process, item) {
+    }
+
+    static async date(step, context, process, item) {
+    }
+
+    static async time(step, context, process, item) {
+    }
+
+    static async duration(step, context, process, item) {
     }
 }
 
