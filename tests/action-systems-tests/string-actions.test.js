05389070f2fdbc9a38cb4e9aa3b4755f14beb0f8
Charles Lemmert <81480730+Astro-Alien@users.noreply.github.com>
2023-03-10 15:55:33 +0200

    committing fix for share reference bug

--- tests/action-systems-tests/string-actions.test.js
+++ tests/action-systems-tests/string-actions.test.js
@@ -1,11 +1,15 @@
-import { assertEquals, assertExists } from "https://deno.land/std@0.147.0/testing/asserts.ts";
+import {assertEquals, assertExists} from "https://deno.land/std@0.147.0/testing/asserts.ts";
 import {init} from "./../mockups/init.js";
+
 await init();
 
 Deno.test("get_query_string - basic url parameters", async () => {
     const context = {}
 
-    await crs.call("string", "get_query_string", {source: "https://localhost:1100/contoso/test/#dashboard/Resource/?remote=Resource&action=GetResourceCollection&id=5000001038", target: "$context.result"}, context);
+    await crs.call("string", "get_query_string", {
+        source: "https://localhost:1100/contoso/test/#dashboard/Resource/?remote=Resource&action=GetResourceCollection&id=5000001038",
+        target: "$context.result"
+    }, context);
 
     assertExists(context.result.remote);
     assertExists(context.result.action);
@@ -18,7 +22,11 @@ Deno.test("get_query_string - basic url parameters", async () => {
 Deno.test("get_query_string - nested url parameter", async () => {
     const context = {}
 
-    await crs.call("string", "get_query_string", {source: "https://localhost:1100/System/Admin/#dashboard/Admin/?remote=TenantIdentityProvider&action=UpdateTenantIdentityProvider&id=1662705947179108&parameters=tenantIdentity=contoso:test", complex_parameters: ["parameters"], target: "$context.result"}, context);
+    await crs.call("string", "get_query_string", {
+        source: "https://localhost:1100/System/Admin/#dashboard/Admin/?remote=TenantIdentityProvider&action=UpdateTenantIdentityProvider&id=1662705947179108&parameters=tenantIdentity=contoso:test",
+        complex_parameters: ["parameters"],
+        target: "$context.result"
+    }, context);
 
     assertExists(context.result.remote);
     assertExists(context.result.action);
@@ -34,7 +42,11 @@ Deno.test("get_query_string - nested url parameter", async () => {
 Deno.test("get_query_string - nested url parameters", async () => {
     const context = {}
 
-    await crs.call("string", "get_query_string", {source: "https://localhost:1100/System/Admin/#dashboard/Admin/?remote=TenantIdentityProvider&action=UpdateTenantIdentityProvider&id=1662705947179108&parameters=tenantIdentity=contoso:test;crudResourceId=1662705947179108", complex_parameters: ["parameters"], target: "$context.result"}, context);
+    await crs.call("string", "get_query_string", {
+        source: "https://localhost:1100/System/Admin/#dashboard/Admin/?remote=TenantIdentityProvider&action=UpdateTenantIdentityProvider&id=1662705947179108&parameters=tenantIdentity=contoso:test;crudResourceId=1662705947179108",
+        complex_parameters: ["parameters"],
+        target: "$context.result"
+    }, context);
 
     assertExists(context.result.remote);
     assertExists(context.result.action);
@@ -52,7 +64,11 @@ Deno.test("get_query_string - nested url parameters", async () => {
 Deno.test("get_query_string - nested url object", async () => {
     const context = {}
 
-    await crs.call("string", "get_query_string", {source: "https://localhost:1100/System/Admin/#dashboard/Admin/?remote=TenantIdentityProvider&action=UpdateTenantIdentityProvider&id=1662705947179108&test=tenantIdentity=contoso:test;crudResourceId=1662705947179108", complex_parameters: ["test"], target: "$context.result"}, context);
+    await crs.call("string", "get_query_string", {
+        source: "https://localhost:1100/System/Admin/#dashboard/Admin/?remote=TenantIdentityProvider&action=UpdateTenantIdentityProvider&id=1662705947179108&test=tenantIdentity=contoso:test;crudResourceId=1662705947179108",
+        complex_parameters: ["test"],
+        target: "$context.result"
+    }, context);
 
     assertExists(context.result.remote);
     assertExists(context.result.action);
@@ -70,7 +86,11 @@ Deno.test("get_query_string - nested url object", async () => {
 Deno.test("get_query_string - multiple nested url object", async () => {
     const context = {}
 
-    await crs.call("string", "get_query_string", {source: "https://localhost:1100/System/Admin/#dashboard/Admin/?remote=TenantIdentityProvider&action=UpdateTenantIdentityProvider&id=1662705947179108&parameters=nestedparam1=1;nestedparam2=2&test=tenantIdentity=contoso:test;crudResourceId=1662705947179108", complex_parameters: ['parameters', 'test'], target: "$context.result"}, context);
+    await crs.call("string", "get_query_string", {
+        source: "https://localhost:1100/System/Admin/#dashboard/Admin/?remote=TenantIdentityProvider&action=UpdateTenantIdentityProvider&id=1662705947179108&parameters=nestedparam1=1;nestedparam2=2&test=tenantIdentity=contoso:test;crudResourceId=1662705947179108",
+        complex_parameters: ['parameters', 'test'],
+        target: "$context.result"
+    }, context);
 
     assertExists(context.result.remote);
     assertExists(context.result.action);
@@ -93,7 +113,11 @@ Deno.test("get_query_string - multiple nested url object", async () => {
 Deno.test("get_query_string - just query section", async () => {
     const context = {}
 
-    await crs.call("string", "get_query_string", {source: "remote=TenantIdentityProvider&action=UpdateTenantIdentityProvider&id=1662705947179108&parameters=tenantIdentity=contoso:test;crudResourceId=1662705947179108", complex_parameters: ['parameters'], target: "$context.result"}, context);
+    await crs.call("string", "get_query_string", {
+        source: "remote=TenantIdentityProvider&action=UpdateTenantIdentityProvider&id=1662705947179108&parameters=tenantIdentity=contoso:test;crudResourceId=1662705947179108",
+        complex_parameters: ['parameters'],
+        target: "$context.result"
+    }, context);
 
     assertExists(context.result.remote);
     assertExists(context.result.action);
@@ -111,7 +135,10 @@ Deno.test("get_query_string - just query section", async () => {
 Deno.test("get_query_string - without nested url parameter", async () => {
     const context = {}
 
-    await crs.call("string", "get_query_string", {source: "https://localhost:1100/System/Admin/#dashboard/Admin/?remote=TenantIdentityProvider&action=UpdateTenantIdentityProvider&id=1662705947179108&parameters=tenantIdentity=contoso:test", target: "$context.result"}, context);
+    await crs.call("string", "get_query_string", {
+        source: "https://localhost:1100/System/Admin/#dashboard/Admin/?remote=TenantIdentityProvider&action=UpdateTenantIdentityProvider&id=1662705947179108&parameters=tenantIdentity=contoso:test",
+        target: "$context.result"
+    }, context);
 
     assertExists(context.result.remote);
     assertExists(context.result.action);
@@ -126,7 +153,10 @@ Deno.test("get_query_string - without nested url parameter", async () => {
 Deno.test("get_query_string - without nested url parameters", async () => {
     const context = {}
 
-    await crs.call("string", "get_query_string", {source: "https://localhost:1100/System/Admin/#dashboard/Admin/?remote=TenantIdentityProvider&action=UpdateTenantIdentityProvider&id=1662705947179108&parameters=tenantIdentity=contoso:test;crudResourceId=1662705947179108", target: "$context.result"}, context);
+    await crs.call("string", "get_query_string", {
+        source: "https://localhost:1100/System/Admin/#dashboard/Admin/?remote=TenantIdentityProvider&action=UpdateTenantIdentityProvider&id=1662705947179108&parameters=tenantIdentity=contoso:test;crudResourceId=1662705947179108",
+        target: "$context.result"
+    }, context);
 
     assertExists(context.result.remote);
     assertExists(context.result.action);
@@ -141,7 +171,10 @@ Deno.test("get_query_string - without nested url parameters", async () => {
 Deno.test("get_query_string - without nested url object", async () => {
     const context = {}
 
-    await crs.call("string", "get_query_string", {source: "https://localhost:1100/System/Admin/#dashboard/Admin/?remote=TenantIdentityProvider&action=UpdateTenantIdentityProvider&id=1662705947179108&test=tenantIdentity=contoso:test;crudResourceId=1662705947179108", target: "$context.result"}, context);
+    await crs.call("string", "get_query_string", {
+        source: "https://localhost:1100/System/Admin/#dashboard/Admin/?remote=TenantIdentityProvider&action=UpdateTenantIdentityProvider&id=1662705947179108&test=tenantIdentity=contoso:test;crudResourceId=1662705947179108",
+        target: "$context.result"
+    }, context);
 
     assertExists(context.result.remote);
     assertExists(context.result.action);
@@ -156,7 +189,11 @@ Deno.test("get_query_string - without nested url object", async () => {
 Deno.test("get_query_string - multiple nested url object with non-matching complex_parameters", async () => {
     const context = {}
 
-    await crs.call("string", "get_query_string", {source: "https://localhost:1100/System/Admin/#dashboard/Admin/?remote=TenantIdentityProvider&action=UpdateTenantIdentityProvider&id=1662705947179108&parameters=nestedparam1=1;nestedparam2=2&test=tenantIdentity=contoso:test;crudResourceId=1662705947179108", complex_parameters:["someOtherParams", "someOtherTest"], target: "$context.result"}, context);
+    await crs.call("string", "get_query_string", {
+        source: "https://localhost:1100/System/Admin/#dashboard/Admin/?remote=TenantIdentityProvider&action=UpdateTenantIdentityProvider&id=1662705947179108&parameters=nestedparam1=1;nestedparam2=2&test=tenantIdentity=contoso:test;crudResourceId=1662705947179108",
+        complex_parameters: ["someOtherParams", "someOtherTest"],
+        target: "$context.result"
+    }, context);
 
     assertExists(context.result.remote);
     assertExists(context.result.action);
@@ -173,7 +210,10 @@ Deno.test("get_query_string - multiple nested url object with non-matching compl
 Deno.test("get_query_string - url with no query string", async () => {
     const context = {}
 
-    await crs.call("string", "get_query_string", {source: "https://localhost:1100/contoso/test/#dashboard/Resource/", target: "$context.result"}, context);
+    await crs.call("string", "get_query_string", {
+        source: "https://localhost:1100/contoso/test/#dashboard/Resource/",
+        target: "$context.result"
+    }, context);
 
     assertEquals(context.result, undefined);
 })
@@ -181,7 +221,10 @@ Deno.test("get_query_string - url with no query string", async () => {
 Deno.test("get_query_string - url with query string, but no parameters", async () => {
     const context = {}
 
-    await crs.call("string", "get_query_string", {source: "https://localhost:1100/contoso/test/#dashboard/Resource/?someOtherValue", target: "$context.result"}, context);
+    await crs.call("string", "get_query_string", {
+        source: "https://localhost:1100/contoso/test/#dashboard/Resource/?someOtherValue",
+        target: "$context.result"
+    }, context);
 
     assertEquals(context.result, undefined);
 })
@@ -189,7 +232,10 @@ Deno.test("get_query_string - url with query string, but no parameters", async (
 Deno.test("get_query_string - url with empty query", async () => {
     const context = {}
 
-    await crs.call("string", "get_query_string", {source: "https://localhost:1100/contoso/test/#dashboard/Resource/?", target: "$context.result"}, context);
+    await crs.call("string", "get_query_string", {
+        source: "https://localhost:1100/contoso/test/#dashboard/Resource/?",
+        target: "$context.result"
+    }, context);
 
     assertEquals(context.result, undefined);
 })
@@ -197,7 +243,10 @@ Deno.test("get_query_string - url with empty query", async () => {
 Deno.test("get_query_string - url with no parameter equal some value", async () => {
     const context = {}
 
-    await crs.call("string", "get_query_string", {source: "https://localhost:1100/contoso/test/#dashboard/Resource/?=someValue", target: "$context.result"}, context);
+    await crs.call("string", "get_query_string", {
+        source: "https://localhost:1100/contoso/test/#dashboard/Resource/?=someValue",
+        target: "$context.result"
+    }, context);
 
     assertEquals(context.result, undefined);
 })
@@ -205,32 +254,44 @@ Deno.test("get_query_string - url with no parameter equal some value", async () 
 Deno.test("get_query_string - nested url with empty parameters", async () => {
     const context = {}
 
-    await crs.call("string", "get_query_string", {source: "https://localhost:1100/System/Admin/#dashboard/Admin/?parameters=", target: "$context.result"}, context);
+    await crs.call("string", "get_query_string", {
+        source: "https://localhost:1100/System/Admin/#dashboard/Admin/?parameters=",
+        target: "$context.result"
+    }, context);
     assertEquals(context.result, undefined);
 })
 
 Deno.test("inflate string", async () => {
-    const template = "${code}: ${description}";
-    let result = await crs.call("string", "inflate", {
-        template: template,
-        parameters: {
-            code: "A11",
-            description: "This is a description"
-        }
-    });
+    const template = "<icon>gear</icon> <bold>[${code}]</bold> ${description}";
 
-    assertEquals(result, "A11: This is a description");
+    const items = [
+            {
+                code: "A11",
+                description: "This is a description",
+                expected: "<icon>gear</icon> <bold>[A11]</bold> This is a description"
+            },
+            {
+                code: "A12",
+                description: "This is another description",
+                expected: "<icon>gear</icon> <bold>[A12]</bold> This is another description"
+            }
+        ]
 
 
-    result = await crs.call("string", "inflate", {
-        template: "<icon>gear</icon> <bold>[${code}]</bold> ${description}",
-        parameters: {
-            code: "A11",
-            description: "Desc"
-        }
-    });
+    const parameters = {
+        code: "$item.code",
+        description: "$item.description"
+    }
 
-    assertEquals(result, "<icon>gear</icon> <bold>[A11]</bold> Desc");
+    for (const item of items) {
+        let result = await crs.call("string", "inflate", {
+            template: template,
+            parameters
+        }, null, null, item);
+
+        assertEquals(result, item.expected);
+    }
+
 })
 
 Deno.test("translate string", async () => {
@@ -240,7 +301,7 @@ Deno.test("translate string", async () => {
     }, "md")
 
     const template = "say &{md.hello} to the &{md.world}."
-    const result = await crs.call("string", "translate", { template })
+    const result = await crs.call("string", "translate", {template})
 
     assertEquals(result, "say Hello to the World.")
 
@@ -262,7 +323,7 @@ Deno.test("template string", async () => {
 
 Deno.test("string slice - simple", async () => {
     const result = await crs.call("string", "slice", {
-        value : "Hello World",
+        value: "Hello World",
         length: 5
     })
 
@@ -271,7 +332,7 @@ Deno.test("string slice - simple", async () => {
 
 Deno.test("string slice - with index", async () => {
     const result = await crs.call("string", "slice", {
-        value : "Hello World",
+        value: "Hello World",
         index: 6,
         length: 5
     })
@@ -281,7 +342,7 @@ Deno.test("string slice - with index", async () => {
 
 Deno.test("string slice - with ellipsis", async () => {
     const result = await crs.call("string", "slice", {
-        value : "Hello World",
+        value: "Hello World",
         length: 5,
         overflow: "ellipsis"
     })
@@ -291,8 +352,8 @@ Deno.test("string slice - with ellipsis", async () => {
 
 Deno.test("string slice - complete", async () => {
     const result = await crs.call("string", "slice", {
-        value : "Test is completed for string slice function",
-        index : 5,
+        value: "Test is completed for string slice function",
+        index: 5,
         length: 16,
         overflow: "ellipsis"
     })
