048894e925c891bb95b68763a140621225f7653b
caperaven <caperaven@gmail.com>
2023-03-13 15:01:23 +0200

    add data processing actions

---
+++ src/action-systems/data-processing-actions.js
@@ -0,0 +1,66 @@
+import init, {unique_values} from "./../bin/data_processing.js";
+
+await init();
+
+export class DataProcessing {
+    static async perform(step, context, process, item) {
+        await this[step.action](step, context, process, item);
+    }
+
+    /**
+     * @method unique_values - returns a map of unique values for each field
+     * It will tell you what the unique values in the array is and how many times it appears
+     * @param step {object} - the step to perform
+     * @param context {object} - the context of the process
+     * @param process {object} - the process currently running
+     * @param item {object} - the item
+     *
+     * @param step.args.source {Array} - the data to check for unique values
+     * @param step.args.fields {Array} - the fields to get the unique values for
+     * @param step.args.target {string} - the target to store the result in
+     * @param step.args.rows {Array} - the rows indexes to check for unique values
+     *
+     * @returns {Promise<void>}
+     *
+     * @example <caption>js example</caption>
+     * const result = await crs.call("data_processing", "unique_values", {
+     *     source: [{value: 1}, {value: 2}, {value: 3}, {value: 3}, {value: null}],
+     *     fields: ["value"]
+     * })
+     *
+     * @example <caption>json example</caption>
+     * {
+     *     "type: "data_processing",
+     *     "action": "unique_values",
+     *     "args": {
+     *         "source": "@context.data",
+     *         "fields": ["value"],
+     *         "target": "@context.result"
+     *     }
+     * }
+     */
+    static async unique_values(step, context, process, item) {
+        const data = await crs.process.getValue(step.args.source, context, process, item);
+        const fields = await crs.process.getValue(step.args.fields, context, process, item);
+        const rows = await crs.process.getValue(step.args.rows, context, process, item);
+
+        if (!Array.isArray(data)) {
+            throw new Error("Fields must be an array");
+        }
+
+        if (!Array.isArray(fields)) {
+            throw new Error("Fields must be an array");
+        }
+
+        const result = unique_values(data, fields, rows);
+
+        if (step.args.target) {
+            await crs.process.setValue(step.args.target, result, context, process, item);
+        }
+
+        return result;
+    }
+}
+
+
+crs.intent.data_processing = DataProcessing;
---
+++ tests/action-systems-tests/data-processing-actions.test.js
@@ -0,0 +1,22 @@
+import { beforeAll, describe, it} from "https://deno.land/std@0.157.0/testing/bdd.ts";
+import {ElementMock} from "../mockups/element-mock.js";
+import { assertEquals, assertNotEquals, assert } from "https://deno.land/std@0.147.0/testing/asserts.ts";
+import "./../mockups/init.js";
+
+beforeAll(async () => {
+    await import("./../../src/action-systems/data-processing-actions.js");
+})
+
+describe("data processing actions tests", () => {
+    it ("unique values", async () => {
+        const result = await crs.call("data_processing", "unique_values", {
+            source: [{value: 1}, {value: 2}, {value: 3}, {value: 3}, {value: null}],
+            fields: ["value"]
+        });
+
+        assertEquals(result["value"]["1"], 1);
+        assertEquals(result["value"]["2"], 1);
+        assertEquals(result["value"]["3"], 2);
+        assertEquals(result["value"]["null"], 1);
+    });
+})
