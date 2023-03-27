43138d58ef32995b5277fe6f28c2f53387f4afd0
caperaven <caperaven@gmail.com>
2023-03-27 17:00:15 +0200

    add tests for drag and drop

---
+++ tests/action-systems-tests/managers/dragdrop-manager/options.test.js
@@ -0,0 +1,17 @@
+import { describe, it } from "https://deno.land/std@0.157.0/testing/bdd.ts";
+import {assert, assertEquals} from "https://deno.land/std@0.147.0/testing/asserts.ts";
+import {init} from "./../../../mockups/init.js";
+import {ensureOptions} from "./../../../../src/action-systems/managers/dragdrop-manager/options.js";
+
+await init();
+
+describe("options tests", () => {
+    it ("ensureOptions - default", async () => {
+        const options = ensureOptions();
+        assertEquals(options.dragQuery, "[draggable='true']");
+        assertEquals(options.drag.placeholderType, "standard");
+        assertEquals(options.drag.clone, "element");
+        assertEquals(options.drop.allowDrop, "[aria-dropeffect]");
+        assertEquals(options.drop.action, "move");
+    });
+})
