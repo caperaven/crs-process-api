221be03b8f30f08f89c46bee7443fe49d9180e87
Gerhard Meyer <gerhard.meyer@pragmaworld.net>
2023-05-08 09:34:46 +0200

    Add debug actions for investigating event listeners

--- src/index.js
+++ src/index.js
@@ -45,6 +45,7 @@ export async function initialize(root) {
     await crs.modules.add("markdown", `${root}/action-systems/markdown-actions.js`);
     await crs.modules.add("html", `${root}/action-systems/html-actions.js`);
     await crs.modules.add("schema", `${root}/action-systems/schema-actions.js`);
+    await crs.modules.add("debug", `${root}/action-systems/debug-actions.js`);
 
     crs.dom = (await crs.modules.get("dom")).DomActions;
 }
