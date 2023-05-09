9e39c03eb3694d2558a33d5f0c9725efe90a7bf2
Gerhard Meyer <gerhard.meyer@pragmaworld.net>
2023-05-09 10:38:21 +0200

    Improved debug actions

--- src/action-systems/debug-actions.js
+++ src/action-systems/debug-actions.js
@@ -17,6 +17,8 @@ export class DebugActions {
      * @param item {Object} - The item that is being processed.
      *
      * @param step.args.log = {Boolean} - If true, the events will be logged to the console when they get added.
+     * @param step.args.tag_name = {String} - If provided will only log events for provide element tag name
+     * @param step.args.event_name = {String} - If provided will only log events for provide event name
      *
      * @example <caption>javascript</caption>
      * await crs.call("debug", "start_monitor_events", {});
@@ -32,6 +34,8 @@ export class DebugActions {
      */
     static async start_monitor_events(step, context, process, item) {
         const log = await crs.process.getValue(step.args.log, context, process, item);
+        const tagName = await crs.process.getValue((step.args.tag_name || "").toUpperCase(), context, process, item);
+        const eventName = await crs.process.getValue(step.args.event_name, context, process, item);
         globalThis.__monitoredEvents = {
             events: [],
             addedCount: 0,
@@ -39,23 +43,37 @@ export class DebugActions {
         };
         EventTarget.prototype.addEventListenerBase = EventTarget.prototype.addEventListener;
         EventTarget.prototype.addEventListener = function (type, listener) {
+            this.addEventListenerBase(type, listener);
+
+            if(tagName && this.tagName !== tagName) return;
+
+            if(eventName && type !== eventName) return;
+
+            const trace = new Error().stack.split("\n");
+
             if (log === true) {
-                console.log("Event added: ", {target: this, type, listener});
+                console.log("Event added: ", {target: this, type, listener,trace });
             }
 
             globalThis.__monitoredEvents.addedCount++;
-            globalThis.__monitoredEvents.events.push({target: this, type: type, listener: listener});
-            this.addEventListenerBase(type, listener);
+            globalThis.__monitoredEvents.events.push({target: this, type: type, listener: listener, trace: trace, path: build_element_path(this)});
         };
 
         EventTarget.prototype.removeEventListenerBase = EventTarget.prototype.removeEventListener;
         EventTarget.prototype.removeEventListener = function (type, listener) {
+            this.removeEventListenerBase(type, listener);
+
+            if(tagName && this.tagName !== tagName) return;
+
+            if(eventName && type !== eventName) return;
+
+            const trace = new Error().stack.split("\n");
+
             if (log === true) {
-                console.log("Event removed: ", {target: this, type, listener});
+                console.log("Event removed: ", {target: this, type, listener, trace});
             }
             globalThis.__monitoredEvents.removedCount++;
             globalThis.__monitoredEvents.events = globalThis.__monitoredEvents.events.filter(e => e.target !== this || e.type !== type || e.listener !== listener);
-            this.removeEventListenerBase(type, listener);
         }
 
         console.log("Started monitoring events.")
@@ -88,17 +106,41 @@ export class DebugActions {
         delete EventTarget.prototype.addEventListenerBase;
         delete EventTarget.prototype.removeEventListenerBase;
 
-        if (globalThis.__monitoredEvents.addedCount !== globalThis.__monitoredEvents.removedCount) {
+        if (globalThis.__monitoredEvents.addedCount > globalThis.__monitoredEvents.removedCount) {
             console.warn(`The number of added events does not match the number of removed events. This may indicate a memory leak. Added: ${globalThis.__monitoredEvents.addedCount}, Removed: ${globalThis.__monitoredEvents.removedCount}`);
         }
         else {
             console.log(`No event leaks found`);
         }
+        const result = globalThis.__monitoredEvents.events.map(e => {
+            return {
+                target: [e.target.tagName, e.target.id || ""].join("#"),
+                type: e.type,
+                path: e.path,
+                trace: e.trace
+            }
+        })
         console.table(globalThis.__monitoredEvents.events);
         globalThis.__monitoredEvents = null;
         delete globalThis.__monitoredEvents;
+        return result;
 
     }
+}
+
+/**
+ * This function returns the path to an element in the DOM tree.
+ * @param element {HTMLElement} - The element to get the path for.
+ * @returns {string} - The path to the element.
+ */
+function build_element_path(element) {
+    if(element instanceof HTMLElement === false) return "";
+    let path = "";
+    while(element) {
+        path = element.tagName + (path ? ">" + path : "");
+        element = element.parentElement;
+    }
+    return path;
 }
 
 crs.intent.debug = DebugActions;
