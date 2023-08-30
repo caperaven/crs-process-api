3f5affa69ce12662a6e9f90f06c71cc8c0b272ef
caperaven <caperaven@gmail.com>
2023-08-30 09:18:38 +0200

    remove wasm target

--- wasm/data-processing/target/release/deps/unicode_ident-31dc9f1f127ba547.d
+++
@@ -1,8 +0,0 @@
-F:\caperaven projects\crs-process-api\wasm\data-processing\target\release\deps\unicode_ident-31dc9f1f127ba547.rmeta: C:\Users\Johan\.cargo\registry\src\github.com-1ecc6299db9ec823\unicode-ident-1.0.8\src\lib.rs C:\Users\Johan\.cargo\registry\src\github.com-1ecc6299db9ec823\unicode-ident-1.0.8\src\tables.rs
-
-F:\caperaven projects\crs-process-api\wasm\data-processing\target\release\deps\libunicode_ident-31dc9f1f127ba547.rlib: C:\Users\Johan\.cargo\registry\src\github.com-1ecc6299db9ec823\unicode-ident-1.0.8\src\lib.rs C:\Users\Johan\.cargo\registry\src\github.com-1ecc6299db9ec823\unicode-ident-1.0.8\src\tables.rs
-
-F:\caperaven projects\crs-process-api\wasm\data-processing\target\release\deps\unicode_ident-31dc9f1f127ba547.d: C:\Users\Johan\.cargo\registry\src\github.com-1ecc6299db9ec823\unicode-ident-1.0.8\src\lib.rs C:\Users\Johan\.cargo\registry\src\github.com-1ecc6299db9ec823\unicode-ident-1.0.8\src\tables.rs
-
-C:\Users\Johan\.cargo\registry\src\github.com-1ecc6299db9ec823\unicode-ident-1.0.8\src\lib.rs:
-C:\Users\Johan\.cargo\registry\src\github.com-1ecc6299db9ec823\unicode-ident-1.0.8\src\tables.rs:
--- wasm/data-processing/target/wasm32-unknown-unknown/release/deps/js_sys-b2b2fd20464b2f98.d
+++
@@ -1,7 +0,0 @@
-F:\caperaven projects\crs-process-api\wasm\data-processing\target\wasm32-unknown-unknown\release\deps\js_sys-b2b2fd20464b2f98.rmeta: C:\Users\Johan\.cargo\registry\src\github.com-1ecc6299db9ec823\js-sys-0.3.61\src\lib.rs
-
-F:\caperaven projects\crs-process-api\wasm\data-processing\target\wasm32-unknown-unknown\release\deps\libjs_sys-b2b2fd20464b2f98.rlib: C:\Users\Johan\.cargo\registry\src\github.com-1ecc6299db9ec823\js-sys-0.3.61\src\lib.rs
-
-F:\caperaven projects\crs-process-api\wasm\data-processing\target\wasm32-unknown-unknown\release\deps\js_sys-b2b2fd20464b2f98.d: C:\Users\Johan\.cargo\registry\src\github.com-1ecc6299db9ec823\js-sys-0.3.61\src\lib.rs
-
-C:\Users\Johan\.cargo\registry\src\github.com-1ecc6299db9ec823\js-sys-0.3.61\src\lib.rs:
