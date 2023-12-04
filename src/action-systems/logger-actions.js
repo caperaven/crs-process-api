e28252a65578efcf2e676755530577c4ccbd2537
caperaven <caperaven@gmail.com>
2023-12-04 23:55:28 +0200

    add logger actions to process api

---
+++ src/action-systems/logger-actions.js
@@ -0,0 +1,73 @@
+export class LoggerActions {
+    #store = {
+        currentLevel: LoggerActions.Levels.INFO,
+        DEBUG: [],
+        INFO: [],
+        WARNING: [],
+        ERROR: []
+    };
+
+    static Levels = {
+        DEBUG: 'DEBUG',
+        INFO: 'INFO',
+        WARNING: 'WARNING',
+        ERROR: 'ERROR'
+    };
+
+    get logs() {
+        return this.#store;
+    }
+
+    #log(level, message) {
+        const levelOrder = {
+            DEBUG: 1,
+            INFO: 2,
+            WARNING: 3,
+            ERROR: 4
+        };
+
+        if (levelOrder[level] >= levelOrder[this.#store.currentLevel]) {
+            const logMessage = `[${new Date().toISOString()}] ${level}: ${message}`;
+            console.log(logMessage);
+            this.#store[level].push(logMessage);
+        }
+    }
+
+    async debug(step, context, process, item) {
+        const message = await crs.process.getValue(step.args.message, context, process, item);
+        this.#log(LoggerActions.Levels.DEBUG, message);
+    }
+
+    async info(step, context, process, item) {
+        const message = await crs.process.getValue(step.args.message, context, process, item);
+        this.#log(LoggerActions.Levels.INFO, message);
+    }
+
+    async warning(step, context, process, item) {
+        const message = await crs.process.getValue(step.args.message, context, process, item);
+        this.#log(LoggerActions.Levels.WARNING, message);
+    }
+
+    async error(step, context, process, item) {
+        const message = await crs.process.getValue(step.args.message, context, process, item);
+        this.#log(LoggerActions.Levels.ERROR, message);
+    }
+
+    async clear() {
+        this.#store.DEBUG = [];
+        this.#store.INFO = [];
+        this.#store.WARNING = [];
+        this.#store.ERROR = [];
+    }
+
+    async print() {
+        console.log(this.#store);
+        return this.#store;
+    }
+
+    async set_level(step, context, process, item) {
+        this.#store.currentLevel = await crs.process.getValue(step.args.level || "INFO", context, process, item);
+    }
+}
+
+crs.intent.logger = new LoggerActions();
---
+++ tests/action-systems-tests/logger-actions.test.js
@@ -0,0 +1,60 @@
+import {assertEquals, assertExists} from "https://deno.land/std@0.147.0/testing/asserts.ts";
+import {beforeAll, beforeEach} from "https://deno.land/std@0.157.0/testing/bdd.ts";
+import {init} from "./../mockups/init.js";
+await init();
+
+beforeAll(async () => {
+    await import ("../../src/action-systems/logger-actions.js");
+})
+
+beforeEach(async () => {
+    await crs.call("logger", "set_level", { level: "INFO" });
+    await crs.call("logger", "clear");
+})
+
+Deno.test("Logger Actions - test sequence of logs - suppress log", async () => {
+    // debug is a lower evel than info so don't log it.
+    await crs.call("logger", "debug", { message: "debug" });
+    assertEquals(crs.intent.logger.logs.DEBUG.length, 0);
+});
+
+Deno.test("Logger Actions - test sequence of logs - positive log", async () => {
+    await crs.call("logger", "set_level", { level: "DEBUG" });
+    await crs.call("logger", "debug", { message: "debug" });
+    assertEquals(crs.intent.logger.logs.DEBUG.length, 1);
+});
+
+Deno.test("Logger Actions - info log", async () => {
+    // debug is a lower evel than info so don't log it.
+    await crs.call("logger", "info", { message: "info" });
+    assertEquals(crs.intent.logger.logs.INFO.length, 1);
+});
+
+Deno.test("Logger Actions - warning log", async () => {
+    // debug is a lower evel than info so don't log it.
+    await crs.call("logger", "warning", { message: "warning" });
+    assertEquals(crs.intent.logger.logs.WARNING.length, 1);
+});
+
+Deno.test("Logger Actions - error log", async () => {
+    // debug is a lower evel than info so don't log it.
+    await crs.call("logger", "error", { message: "error" });
+    assertEquals(crs.intent.logger.logs.ERROR.length, 1);
+});
+
+Deno.test("Logger Actions - print", async () => {
+    // stub out console.log
+    let message;
+
+    globalThis.console = {
+        log: (msg) => message = msg
+    }
+
+    await crs.call("logger", "info", { message: "info" });
+    await crs.call("logger", "print");
+
+    assertExists(message);
+});
+
+
+
